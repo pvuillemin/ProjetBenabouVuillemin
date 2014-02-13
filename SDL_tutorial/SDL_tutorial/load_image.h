34c052fcb9a45201b37d955f389187f5c846b8a4
pvuillemin <vuillemin_pierre@yahoo.fr>
2014-02-13 02:27:25 +0100

    Revert "Début du gui"
    This reverts commit 5be3ab91e300fbcfbac120dc7582cd88ba6883f0.

--- SDL_tutorial/SDL_tutorial/load_image.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "SDL.h"
-#include "SDL_image.h"
-#include <string>
-SDL_Surface* load_image(std::string filename);
-void Apply_surface(int x, int y, SDL_Surface* source, SDL_Surface* destination);
